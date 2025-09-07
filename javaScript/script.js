19ad032eda926802ff986ff2f20f593d8d0429ed
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-09-07 13:01:04 -0600

    Fix: Tamaño de imagen de sobre mí

--- javaScript/script.js
+++ javaScript/script.js
@@ -17,3 +17,20 @@ enlaces.forEach(enlace => {
     });
 })
 });
+
+new Swiper('.container_cards', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+

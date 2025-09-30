154d6f76530efbf12c5a449a85a62ec71b84e1ab
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-09-29 21:14:55 -0600

    Fix: Se arreglaron errores de JavaScrip

--- javaScript/script.js
+++ javaScript/script.js
@@ -13,6 +13,7 @@ nav_toggle.onclick = function() {
 }
 const enlaces = document.querySelectorAll('.nav-movile a');
 
+    //para el menu pcs
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         const id = entry.target.getAttribute('id');
@@ -25,16 +26,7 @@ const observer = new IntersectionObserver((entries) => {
                 navLink.classList.add('selected');
             }
         }
-        const id_movile = entry.target.getAttribute('id');
-        const navLink_movile = document.querySelector(`.nav-movile a[href="#${id_movile}"]`);
-        if (entry.isIntersecting) {
-            document.querySelectorAll('.nav-movile a').forEach(link => {
-                link.classList.remove('selected');
-            });
-            if (navLink_movile) {
-                navLink_movile.classList.add('selected');
-            }
-        }
+
     });
 }, {
     rootMargin: "0% 0px 30% 0px", threshold: 0.5 
@@ -42,37 +34,23 @@ const observer = new IntersectionObserver((entries) => {
 //Falta arreglar el observer del menu movil
 const observer_movile = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        const navLink = document.querySelector(`..nav-movile a[href="#${id}"]`);
+        const id_movile = entry.target.getAttribute('id');
+        const navLink_movile = document.querySelector(`.nav-movile a[href="#${id_movile}"]`);
         if (entry.isIntersecting) {
-            document.querySelectorAll('.nav-movile a').forEach(link => {
-                link.classList.remove('selected');
+            document.querySelectorAll('.nav-movile a').forEach(link_movile => {
+                link_movile.classList.remove('selected');
             });
-            if (navLink) {
-                navLink.classList.add('selected');
+            if (navLink_movile) {
+                navLink_movile.classList.add('selected');
             }
         }
     });
 }, {
     rootMargin: "0% 0px 10% 0px", threshold: 0.5 
 });
-// const nav_desktop = document.querySelector('.nav-desktop a');
-// //Para que las secciones en el nav vayan cambiando de color al hacer scroll
-// //const menuLinks = document.querySelectorAll(`.nav-movile a[href="#"]`);
-// const menuLinks = document.querySelectorAll(`.nav_desktop a[href="#"]`);
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         const id = entry.target.getAttribute("id");
-//         const menuLinks = document.querySelector(`.nav_desktop a[href="#${id}"]`);
-
-//       if (entry.isIntersecting) {
-//         document.querySelectorAll(".nav_desktop a.selected").classList.remove('selected');
-//         menuLinks.classList.add("selected");
-//       }
-//     });
-// }, { rootMargin: "-30% 0px -70% 0px", threshold: 0.5 });
 
 
+//Para cerrar el menu movil al dar click en un enlace
 enlaces.forEach(enlace => {
     enlace.addEventListener('click', () => {
         nav_movile.classList.remove('open');

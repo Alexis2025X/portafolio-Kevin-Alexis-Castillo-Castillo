eaebafec6df9a235e0b972c2c7b4055db5c20fdd
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-12-08 17:38:55 -0600

    Feat: Sección de reconocimientos y  reseñas agregadas

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,111 +1,125 @@
-document.addEventListener('DOMContentLoaded', function() {
-const nav_toggle = document.querySelector('.nav-toggle');
-const nav_toggle_icon = document.querySelector('.nav-toggle i');
-const nav_movile = document.querySelector('.nav-movile');
+document.addEventListener('DOMContentLoaded', function () {
+    const nav_toggle = document.querySelector('.nav-toggle');
+    const nav_toggle_icon = document.querySelector('.nav-toggle i');
+    const nav_movile = document.querySelector('.nav-movile');
 
 
-// para el menu hamburguesa
-nav_toggle.onclick = function() {
-    nav_movile.classList.toggle('open');
-    const isOpen = nav_movile.classList.contains('open');
-    nav_toggle_icon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+    // para el menu hamburguesa
+    nav_toggle.onclick = function () {
+        nav_movile.classList.toggle('open');
+        const isOpen = nav_movile.classList.contains('open');
+        nav_toggle_icon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 
-}
-const enlaces = document.querySelectorAll('.nav-movile a');
+    }
+    const enlaces = document.querySelectorAll('.nav-movile a');
 
     //para el menu pcs
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        const navLink = document.querySelector(`.nav-desktop a[href="#${id}"]`);
-        if (entry.isIntersecting) {
-            document.querySelectorAll('.nav-desktop a').forEach(link => {
-                link.classList.remove('selected');
-            });
-            if (navLink) {
-                navLink.classList.add('selected');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            const navLink = document.querySelector(`.nav-desktop a[href="#${id}"]`);
+            if (entry.isIntersecting) {
+                document.querySelectorAll('.nav-desktop a').forEach(link => {
+                    link.classList.remove('selected');
+                });
+                if (navLink) {
+                    navLink.classList.add('selected');
+                }
+            }
+
+        });
+    }, {
+        rootMargin: "0% 0px 30% 0px", threshold: 0.5
+    });
+    //Falta arreglar el observer del menu movil
+    const observer_movile = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            const id_movile = entry.target.getAttribute('id');
+            const navLink_movile = document.querySelector(`.nav-movile a[href="#${id_movile}"]`);
+            if (entry.isIntersecting) {
+                document.querySelectorAll('.nav-movile a').forEach(link_movile => {
+                    link_movile.classList.remove('selected');
+                });
+                if (navLink_movile) {
+                    navLink_movile.classList.add('selected');
+                }
+            }
+        });
+    }, {
+        rootMargin: "0% 0px 10% 0px", threshold: 0.5
+    });
+
+
+    //Para cerrar el menu movil al dar click en un enlace
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', () => {
+            nav_movile.classList.remove('open');
+            nav_toggle_icon.classList = 'fa-solid fa-bars';
+        });
+        const hash = enlace.getAttribute('href');
+        const target = document.querySelector(hash);
+        if (hash.startsWith('#')) {
+            if (target) {
+                observer.observe(target);
+                observer_movile.observe(target);
             }
         }
-
-    });
-}, {
-    rootMargin: "0% 0px 30% 0px", threshold: 0.5 
-});
-//Falta arreglar el observer del menu movil
-const observer_movile = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        const id_movile = entry.target.getAttribute('id');
-        const navLink_movile = document.querySelector(`.nav-movile a[href="#${id_movile}"]`);
-        if (entry.isIntersecting) {
-            document.querySelectorAll('.nav-movile a').forEach(link_movile => {
-                link_movile.classList.remove('selected');
-            });
-            if (navLink_movile) {
-                navLink_movile.classList.add('selected');
-            }
-        }
-    });
-}, {
-    rootMargin: "0% 0px 10% 0px", threshold: 0.5 
-});
-
-
-//Para cerrar el menu movil al dar click en un enlace
-enlaces.forEach(enlace => {
-    enlace.addEventListener('click', () => {
-        nav_movile.classList.remove('open');
-        nav_toggle_icon.classList = 'fa-solid fa-bars';
-    });
-    const hash = enlace.getAttribute('href');
-    const target = document.querySelector(hash);
-    if (hash.startsWith('#')) {
-        if (target) {
-            observer.observe(target);
-            observer_movile.observe(target);
-        }
-    }
-})
-
-//Botón traductor
-const button_traductor = document.getElementById('button-traductor');
-button_traductor.addEventListener('click', () => {
-    if(button_traductor.innerHTML === `<i class="fa-solid fa-globe"></i>${button_traductor.innerText = "Español"}`) {
-        //button_traductor.innerText = 'Español';
-
-        button_traductor.innerHTML = `<i class="fa-solid fa-globe"></i>${button_traductor.innerText = "Ingles"}`;
-        button_traductor.setAttribute('data-lenguaje', 'es');
-            //alert('Función en desarrollo');
-    } else {
-        button_traductor.innerHTML = `<i class="fa-solid fa-globe"></i>${button_traductor.innerText = "Español"}`;
-        button_traductor.setAttribute('data-lenguaje', 'en');
-        //button_traductor.innerText = 'Ingles';
-            //alert('Función en terminada');
-    }
-});
-
-
-const traductor = document.querySelectorAll("[data-lenguaje]");
-const textToChange = document.querySelectorAll("[data-section]");
-traductor.forEach((button) => {
-    button.addEventListener('click', () => {
-        //const lang = button.getAttribute('data-lenguaje');
-        //console.log(button.getAttribute('data-lenguaje'));
-        fetch(`json/${button.getAttribute('data-lenguaje')}.json`)
-            .then(res => res.json()) 
-            .then(data => {
-                textToChange.forEach(element => {
-                    const section = element.getAttribute('data-section');
-                    const value = element.getAttribute('data-value');
-                    element.innerHTML = data[section][value];
-                    element.placeholder = data[section][value]
-                    
-                    // console.log(element.innerHTML = data[section][value])
-                    //console.log(element.ariaPlaceholder = data[section][value])
-                })
-            })
     })
-})
 
+    //Botón traductor
+    const button_traductor = document.getElementById('button-traductor');
+    button_traductor.addEventListener('click', () => {
+        if (button_traductor.innerHTML === `<i class="fa-solid fa-globe"></i>${button_traductor.innerText = "Español"}`) {
+            //button_traductor.innerText = 'Español';
+
+            button_traductor.innerHTML = `<i class="fa-solid fa-globe"></i>${button_traductor.innerText = "Ingles"}`;
+            button_traductor.setAttribute('data-lenguaje', 'es');
+            //alert('Función en desarrollo');
+        } else {
+            button_traductor.innerHTML = `<i class="fa-solid fa-globe"></i>${button_traductor.innerText = "Español"}`;
+            button_traductor.setAttribute('data-lenguaje', 'en');
+            //button_traductor.innerText = 'Ingles';
+            //alert('Función en terminada');
+        }
+    });
+
+
+    const traductor = document.querySelectorAll("[data-lenguaje]");
+    const textToChange = document.querySelectorAll("[data-section]");
+    traductor.forEach((button) => {
+        button.addEventListener('click', () => {
+            //const lang = button.getAttribute('data-lenguaje');
+            //console.log(button.getAttribute('data-lenguaje'));
+            fetch(`json/${button.getAttribute('data-lenguaje')}.json`)
+                .then(res => res.json())
+                .then(data => {
+                    textToChange.forEach(element => {
+                        const section = element.getAttribute('data-section');
+                        const value = element.getAttribute('data-value');
+                        element.innerHTML = data[section][value];
+                        element.placeholder = data[section][value]
+
+                        // console.log(element.innerHTML = data[section][value])
+                        //console.log(element.ariaPlaceholder = data[section][value])
+                    })
+                })
+        })
+    })
+    document.getElementById("imagenZoom1").addEventListener("click", zoomImagen)
+    document.getElementById("imagenZoom2").addEventListener("click", zoomImagen)
+    // content_img.addEventListener('click', zoomImagen)
+    function zoomImagen(event) {
+        let elemento = event.target;
+        if (elemento.tagName === 'IMG') {
+            elemento = elemento.parentElement;
+        }
+        const tieneClase = elemento.classList.contains('imagen_zoom');
+        if (tieneClase) {
+            elemento.classList.remove('imagen_zoom')
+        } else {
+            elemento.classList.add('imagen_zoom')
+        }
+    }
 
 
 
